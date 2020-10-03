0c7df7da590962688a2eaf65eca6bb75d7888db8
Tintin <tanner@schenck.xyz>
2020-10-03 00:57:43 -0400

    questions created for user to answer in cli to generate readme

--- index.js
+++ index.js
@@ -16,21 +16,42 @@ inquirer.prompt([
     },
     {
         type: "input",
-        message: "What are the steps required to install your project?",
+        message: "Steps required to install your project?",
         name: "install"
-        
+
     },
     {
         type: "input",
-        message: "Provide instructions and examples for use. Include screenshots as needed.",
+        message: "Provide instructions and examples for use",
         name: "use"
 
     },
     {
+        type: "list",
+        choices: ["GNU GPLv3", "MIT License", "Apache License 2.0"],
+        name: "license"
 
-    }, 
-
-
+    },
+    {
+        type: "input",
+        message: "Who contributed to the project?",
+        name: "credit"
+    },
+    {
+        type: "input",
+        message: "What command do you use to run tests",
+        name: "test"
+    },
+    {
+        type: "input",
+        message: "What is your GitHub username",
+        name: "github"
+    },
+    {
+        type: "input",
+        message: "What is your email?",
+        name: "email"
+    }
 
 ])
     .then(function (answers) {
