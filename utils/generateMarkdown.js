30b0840f0c546f270f7cc6cfb2f0498fc0e88328
Tintin <tanner@schenck.xyz>
2020-10-03 01:09:59 -0400

    badge icons

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -7,31 +7,20 @@ function generateMarkdown(answers) {
   ## Description
   ${answers.description} 
 
-  ### Installation
-
-
-What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.
+  ## Installation
+  Run this to install and get started
+  <pre><code>${answers.install}</code></pre>
 
 
 ## Usage 
-
-Provide instructions and examples for use. Include screenshots as needed. 
-
+${answers.use}
 
 ## Credits
+Contributors: ${answers.credit}
 
-List your collaborators, if any, with links to their GitHub profiles.
+##License
+This program is licensed udner <code>${answers.license}</code>
 
-If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
-
-If you followed tutorials, include links to those here as well.
-
-
-
-## License
-![Picture of ${answers.license}](https://img.shields.io/badge/license-${answers.license}-blue.svg)
-
-The last section of a good README is a license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, use [https://choosealicense.com/](https://choosealicense.com/)
 
 
 ---
